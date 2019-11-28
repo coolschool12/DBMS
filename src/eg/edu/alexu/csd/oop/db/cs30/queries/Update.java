3e1bb4eafc9e11bb64e20439a9c43b76cc77ce14
Zyad Ayman <zyad_zox@hotmail.com>
2019-11-28 20:22:51 +0200

    Update regex

--- src/eg/edu/alexu/csd/oop/db/cs30/queries/Update.java
+++ src/eg/edu/alexu/csd/oop/db/cs30/queries/Update.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 public class Update implements Query {
     @Override
     public boolean isCorrect(String query) {
-        Pattern pattern = Pattern.compile("(^\\s*UPDATE\\s+[^\\s]+\\s+SET\\s+([^\\s]+\\s*=\\s*[^\\s]+\\s*,\\s*)*([^\\s]+\\s*=\\s*[^\\s]+)((\\s+WHERE\\s+[^\\s]+)|\\s*)(\\s*;\\s*|\\s*)$)",Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("(^\\s*UPDATE\\s+[^\\s]+\\s+SET\\s+([^\\s]+\\s*=\\s*[^\\s]+\\s*,\\s*)*([^\\s]+\\s*=\\s*[^\\s]+)((\\s+WHERE\\s+.+)|\\s*)(\\s*;\\s*|\\s*)$)",Pattern.CASE_INSENSITIVE);
         return pattern.matcher(query).matches();
     }
 
