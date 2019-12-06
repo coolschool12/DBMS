b7d1262cb135e7e699fcd568148bdbd8137ef069
ziadabdelkader <zyad012ziad012@gmail.com>
2019-12-06 10:18:13 +0200

    add logger

--- src/eg/edu/alexu/csd/oop/db/cs30/DataBaseGenerator.java
+++ src/eg/edu/alexu/csd/oop/db/cs30/DataBaseGenerator.java
@@ -4,11 +4,13 @@ import eg.edu.alexu.csd.oop.db.Database;
 import eg.edu.alexu.csd.oop.db.cs30.queries.ExtractData;
 import eg.edu.alexu.csd.oop.db.cs30.queries.Query;
 import eg.edu.alexu.csd.oop.db.cs30.queries.QueryBuilder;
+import sun.rmi.runtime.Log;
 
 import java.sql.SQLException;
 import java.sql.SQLTimeoutException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class DataBaseGenerator implements Database {
 
@@ -17,7 +19,7 @@ public class DataBaseGenerator implements Database {
     private ExtractData extractData;
     private String pathToDatabases = "databases" + System.getProperty("file.separator") + ((int)(Math.random() * 100));
     private static Database database = null;
-
+    private Logger myLogger;
     private static String[] selectedColumnNames;
     private static Integer[] selectedColumnTypes;
 
@@ -25,6 +27,8 @@ public class DataBaseGenerator implements Database {
     {
         extractData = ExtractData.makeInstance();
         dataBases = new ArrayList<>();
+        System.setProperty("java.util.logging.config.file", "logging.properties");
+        myLogger=Logger.getLogger(DataBaseGenerator.class.getName());
     }
 
     public static Database makeInstance()
@@ -88,8 +92,12 @@ public class DataBaseGenerator implements Database {
         if (activeDataBase == null)
             throw new SQLException("THERE IS NO AN ACTIVE DATABASE!!!");
 
-        if (exp.isCorrect(query) && exp.getId() == 7)
-            return activeDataBase.select((HashMap<String, Object>) extractData.SelectedProperties(query));
+        if (exp.isCorrect(query) && exp.getId() == 7){
+            Object[][] result=activeDataBase.select((HashMap<String, Object>) extractData.SelectedProperties(query));
+            myLogger.info("selection Query performed on table "+extractData.getTableName(query));
+            return result;
+        }
+
         else
             throw new SQLException("you cant perform the select query");
     }
@@ -103,14 +111,24 @@ public class DataBaseGenerator implements Database {
         if (activeDataBase == null || !exp.isCorrect(query))
             throw new SQLException("THERE IS NO AN ACTIVE DATABASE!!!");
 
-        if (exp.getId() == 4)
-            return activeDataBase.editTable(extractData.getObjectsToInsert(query), extractData.getTableName(query));
+        if (exp.getId() == 4){
+            int result=activeDataBase.editTable(extractData.getObjectsToInsert(query), extractData.getTableName(query));
+            myLogger.info(result+" record is added to table "+extractData.getTableName(query));
+            return result;
+        }
 
-        else if (exp.getId() == 5)
-            return activeDataBase.delete((HashMap<String, Object>) extractData.DeleteProperties(query));
+        else if (exp.getId() == 5){
+            int result=activeDataBase.delete((HashMap<String, Object>) extractData.DeleteProperties(query));
+            myLogger.info(result+" records are deleted from table "+extractData.getTableName(query));
+            return result;
+        }
 
-        else if (exp.getId() == 6)
-            return activeDataBase.update((HashMap<String, Object>) extractData.UpadteProperties(query));
+
+        else if (exp.getId() == 6){
+            int result=activeDataBase.update((HashMap<String, Object>) extractData.UpadteProperties(query));;
+            myLogger.info(result+" records are updated in table "+extractData.getTableName(query));
+            return result;
+        }
 
         else
             throw new SQLException("Query is incorrect !! ");
@@ -145,6 +163,7 @@ public class DataBaseGenerator implements Database {
         if (exp.getId() == 2)
         {
             if (!isFound) {
+                myLogger.warning("trying to drop database "+dataBaseName+" which doesn't exist.");
                 return false;
                // throw new SQLException("THE TABLE DOESNT EXIST !!!!");
             }
@@ -152,6 +171,7 @@ public class DataBaseGenerator implements Database {
             {
                 TableFactory.delete(activeDataBase.getPath());
                 dataBases.remove(activeDataBase);
+                myLogger.info("database "+activeDataBase.getName()+" is droped.");
                 activeDataBase = null;
                 return true;
             }
@@ -161,10 +181,14 @@ public class DataBaseGenerator implements Database {
                 if (!isFound) {
                     activeDataBase = new MyDataBase(dataBaseName, pathToDatabases);
                     dataBases.add(activeDataBase);
+                    myLogger.info("Data base "+activeDataBase.getName() +" added.");
                     return true;
                 }
-                else
+                else{
+                    myLogger.info("Data base " +dataBaseName +" found.");
                     return true;
+                }
+
             }
             catch (SQLTimeoutException e) {
                 this.activeDataBase = oldActiveDatabase;
@@ -178,12 +202,22 @@ public class DataBaseGenerator implements Database {
             throw new SQLException();
 
         String tableName = extractData.getTableName(query);
-        if (exp.getId() == 3)
-            return activeDataBase.removeTable(tableName);
-
-        else
-            return activeDataBase.addTable(extractData.getContentsOfTableQuery(query), tableName);
-
+        if (exp.getId() == 3) {
+            if (activeDataBase.removeTable(tableName)) {
+                myLogger.info("table " + tableName + " removed from database " + activeDataBase.getName() + ".");
+                return true;
+            }
+            myLogger.warning("table " + tableName + " failed to removed.");
+            return false;
+        }
+        else{
+            if (activeDataBase.addTable(extractData.getContentsOfTableQuery(query), tableName)) {
+                myLogger.info("table " + tableName + " added to database " + activeDataBase.getName() + ".");
+                return true;
+            }
+            myLogger.warning("table " + tableName + "failed to add.");
+            return false;
+        }
     }
 
     private boolean searchForThatName(String databaseName)
@@ -200,5 +234,7 @@ public class DataBaseGenerator implements Database {
     }
 
 
-
+    public Logger getMyLogger() {
+        return myLogger;
+    }
 }
